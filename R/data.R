1b1de900058a1bab5189b9bcf185b5a637c1ef73
Elina Gómez <elinagomezbonaglia@gmail.com>
2020-08-15 14:26:30 -0300

    data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+
+#' @title Base Covid19 
+#'
+#' @description Base de datos de evolución covid19 al 
+#'
+#' @format Base de datos al 04/08/2020
+#' 
+#' @source <https://github.com/UMAD-FCS>
+"covid"
+
